10a100e0e108da575bad929e7e9e23acc47ad98c
thisconnect <p7tryk@gmail.com>
2020-01-10 14:32:00 +0100

    verbatim od tokarza fix

--- pthreads/odtokarz.c
+++ pthreads/odtokarz.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void *PrintHello(void *message)
+void *printMessage(void *message)
 {
   
   char * string = (char*)message;
@@ -20,12 +20,12 @@ int main (int argc, char *argv[])
   const char *message2 = "thread2";
   int iret1, iret2;
 
-  iret1 = pthread_create(&thread1, NULL, PrintHello, (void *)message1);
+  iret1 = pthread_create(&thread1, NULL, printMessage, (void *)message1);
   if (iret1){
     fprintf(stderr,"ERROR; return code from pthread_create() is %d\n", iret1);
     exit(EXIT_FAILURE);
   }
-  iret2 = pthread_create(&thread2, NULL, PrintHello, (void *)message2);
+  iret2 = pthread_create(&thread2, NULL, printMessage, (void *)message2);
   if (iret2){
     fprintf(stderr,"ERROR; return code from pthread_create() is %d\n", iret2);
     exit(EXIT_FAILURE);
