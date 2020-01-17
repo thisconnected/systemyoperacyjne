13f8634182bb8ad0255fd000b527a68c25b12f21
thisconnect <p7tryk@gmail.com>
2020-01-17 14:06:47 +0100

    simple kernel module

---
+++ kernelmodule/simp.c
@@ -0,0 +1,21 @@
+#include <linux/module.h>
+#include <linux/moduleparam.h>
+#include <linux/init.h>
+#include <linux/kernel.h>   
+ 
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Patryk Kaniewski");
+ 
+static int simple_init(void)
+{
+	printk(KERN_ALERT "inicjalizacja");
+	return 0;
+}
+ 
+static void simple_cleanup(void)
+{
+	printk(KERN_WARNING "zamkniecie");
+}
+ 
+module_init(simple_init);
+module_exit(simple_cleanup);
